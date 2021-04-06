66ad7b7282a70f758875e580f4ed6a5a2ae4d25d
Scott Smith <stsmith@callutheran.edu>
2021-04-06 16:38:59 -0700

    first!

---
+++ parts/sidebar-cta.php
@@ -0,0 +1,6 @@
+<div class="btn-group">
+	<a href="/admission/undergraduate/apply/" class="btn blue block btn-arrow-right" onclick="ga('send', 'event', 'UGadmission', 'Click - Majors CTA', 'How to Apply');">How to Apply</a>
+	<a href="/financial-aid/affordability/" class="btn blue block btn-arrow-right" onclick="ga('send', 'event', 'UGadmission', 'Click - Majors CTA', 'Tuition &amp; Financial Aid');">Tuition &amp; Financial Aid</a>
+	<a href="/admission/undergraduate/contact/request-info.html" class="btn blue block btn-arrow-right" onclick="ga('send', 'event', 'UGadmission', 'Click - Majors CTA', 'Request Info');">Request Info</a>
+	<a href="/admission/undergraduate/visit/schedule.html" class="btn blue block btn-arrow-right" onclick="ga('send', 'event', 'UGadmission', 'Click - Majors CTA', 'Visit Us');">Visit Us</a>
+</div>
