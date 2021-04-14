76f1b5e82f79869d2bcd8fe0fcdf551efcd1177f
Scott Smith <stsmith@callutheran.edu>
2021-04-14 14:09:08 -0700

    check for profile photo

--- parts/faculty-item.php
+++ parts/faculty-item.php
@@ -1,6 +1,11 @@
+<?php
+	// check for profile photo
+	$img = (isset($faculty_item['has_image']) && $faculty_item['has_image'] == 'true' ) ? 'https://earth.callutheran.edu/images/profile_photos/150/'.$faculty_item['id'].'.jpg' : 'https://www.callutheran.edu/academics/majors/_resources/placeholder.png';
+?>
+
 
 <a class="profile" href="/faculty/profile.html?id=<?php echo $faculty_item['username']; ?>" title="<?php echo $faculty_item['name']; ?>">
-	<div class="square-profile ll-bg" style="background-image: url('https://earth.callutheran.edu/images/profile_photos/150/<?php echo $faculty_item['id']; ?>.jpg');"></div>
+	<div class="square-profile ll-bg" style="background-image: url('<?php echo $img; ?>');"></div>
 	<header><?php echo $faculty_item['name']; ?></header>
 	<p><?php echo $faculty_item['title']; ?></p>
 </a>
