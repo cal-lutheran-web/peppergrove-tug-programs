b44785dfda83c2289d536c4f8343e0b05466bdb4
Scott Smith <stsmith@callutheran.edu>
2021-04-16 09:45:58 -0700

    removed tall page hero class in minors

--- parts/page-hero-minor.php
+++ parts/page-hero-minor.php
@@ -1,4 +1,4 @@
-<section class="page-hero tall">
+<section class="page-hero">
 	<div class="page-hero-heading-wrapper">
 		<h1 class="page-hero-heading"><?php the_title(); ?></h1>
 		<h2 class="page-hero-subheading">Minor</h2>
