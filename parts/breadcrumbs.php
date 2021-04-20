43bad1cb49bf19e2ef3a25e2320a44f2460d0a7a
Scott Smith <stsmith@callutheran.edu>
2021-04-20 09:19:00 -0700

    use conejo function to generate breadcrumbs

--- parts/breadcrumbs.php
+++ parts/breadcrumbs.php
@@ -1,17 +1,18 @@
-<div class="breadcrumb-row">
-	<div class="container">
-		<div class="row">
-			<div class="col-sm-12">
-				<!--  BREADCRUMB -->
-				<nav id="top-breadcrumb" class="breadcrumb">
-					<ul>
-						<li><a href="/" title="Home">Home</a></li>
-						<li><a href="/academics/" title="Academics">Academics</a></li>
-						<li><a href="/academics/majors/" title="Undergraduate Majors &amp; Minors">Undergraduate Majors &amp; Minors</a></li>
-						<li><span><?php the_title(); ?></span></li>
-					</ul>
-				</nav>
-			</div>
-		</div>
-	</div>
-</div>
+<!-- breadcrumbs -->
+<?php
+	
+	echo breadcrumbs(array(
+		array(
+			'link' => "/academics/",
+			"name" => "Academics"
+		),
+		array(
+			"link" => "/academics/majors/",
+			"name" => "Undergraduate Majors & Minors"
+		),
+		array(
+			"name" => get_the_title()
+		)
+	));
+
+?>
