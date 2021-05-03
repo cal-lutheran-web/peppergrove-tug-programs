e35dda809753b5158566c5de783f761893c9d356
Scott Smith <stsmith@callutheran.edu>
2021-05-03 10:26:59 -0700

    changed markup to use row inside of section

--- single-major.php
+++ single-major.php
@@ -65,75 +65,81 @@
 
 		<section id="curriculum">
 			<div class="container">
-				<div class="col-sm-12"><div class="section-title">The Curriculum</div></div>
-				<div class="col-md-8 col-sm-9">
-				
-					<?php the_field('curriculum'); ?>
-				
+				<div class="row">
+					<div class="col-sm-12">
+						<div class="section-title">The Curriculum</div>
+					</div>
 				</div>
-				<div class="col-md-4 col-sm-3">
+				<div class="row">
+					<div class="col-md-8 col-sm-9">
+				
+						<?php the_field('curriculum'); ?>
 					
-					<hr class="gold hide-on-desktop">
-					
-					<h6>Programs</h6>
-					<p><?php echo program_format_sentence($post); ?></p>
-					<br>
-
-					
-					<?php
-						$concentrations = get_field('concentrations');
-
-						if(!empty($concentrations)){ ?>
+					</div>
+					<div class="col-md-4 col-sm-3">
 						
-						<h6>Concentrations</h6>
-						<ul>
-							<?php foreach($concentrations as $key=>$c){
-								echo '<li>'.$c->name.'</li>';
-							} ?>
-						</ul>
+						<hr class="gold hide-on-desktop">
+						
+						<h6>Programs</h6>
+						<p><?php echo program_format_sentence($post); ?></p>
+						<br>
+
+						
+						<?php
+							$concentrations = get_field('concentrations');
+
+							if(!empty($concentrations)){ ?>
+							
+							<h6>Concentrations</h6>
+							<ul>
+								<?php foreach($concentrations as $key=>$c){
+									echo '<li>'.$c->name.'</li>';
+								} ?>
+							</ul>
+
+							<?php } ?>
+
+						
+						<?php
+							$related_minors = get_field('related_minors');
+
+							if(!empty($related_minors)){ ?>
+							
+							<h6>Popular Minor Pairings</h6>
+							<ul class="special-list">
+
+								<?php foreach($related_minors as $key=>$item){
+									echo '<li><a href="/academics/minors/'.$item->post_name.'">'.$item->post_title.'</a></li>';
+								} ?>
+
+							</ul>
+							<br>	
 
 						<?php } ?>
-
-					
-					<?php
-						$related_minors = get_field('related_minors');
-
-						if(!empty($related_minors)){ ?>
-						
-						<h6>Popular Minor Pairings</h6>
-						<ul class="special-list">
-
-							<?php foreach($related_minors as $key=>$item){
-								echo '<li><a href="/academics/minors/'.$item->post_name.'">'.$item->post_title.'</a></li>';
-							} ?>
-
-						</ul>
-						<br>	
-
-					<?php } ?>
-								
-
-					<div class="row">
-						<div class="col-sm-12 col-md-9">
-					
-							<div class="btn-group">
-								<?php if(get_field('catalog_url')){ ?>
-									<a class="btn gold icon-check block btn-arrow-right" href="<?php the_field('catalog_url'); ?>" title="Course Requirements">Course Requirements</a>
 									
-									<a class="btn gold icon-course-requirements block btn-arrow-right" href="<?php the_field('catalog_url'); ?>#courseinventory" title="Course Descriptions">Course Descriptions</a>		
-								<?php } ?>
 
-								<?php if(get_field('department_url')){ ?>
-									<a class="btn gold icon-web block btn-arrow-right" href="<?php the_field('department_url'); ?>" title="Department Website">Department Website</a>
-								<?php } ?>			
+						<div class="row">
+							<div class="col-sm-12 col-md-9">
+						
+								<div class="btn-group">
+									<?php if(get_field('catalog_url')){ ?>
+										<a class="btn gold icon-check block btn-arrow-right" href="<?php the_field('catalog_url'); ?>" title="Course Requirements">Course Requirements</a>
+										
+										<a class="btn gold icon-course-requirements block btn-arrow-right" href="<?php the_field('catalog_url'); ?>#courseinventory" title="Course Descriptions">Course Descriptions</a>		
+									<?php } ?>
+
+									<?php if(get_field('department_url')){ ?>
+										<a class="btn gold icon-web block btn-arrow-right" href="<?php the_field('department_url'); ?>" title="Department Website">Department Website</a>
+									<?php } ?>			
+								</div>
+								<br>
+								
+								<?php get_template_part('parts/sidebar-cta'); ?>
+								
 							</div>
-							<br>
-							
-							<?php get_template_part('parts/sidebar-cta'); ?>
-							
 						</div>
+						
 					</div>
-					
 				</div>
 			</div>
 		</section>
@@ -162,29 +168,34 @@
 		
 	<section id="experience" style="padding-bottom:0;">
 		<div class="container">
-			<div class="col-sm-12">
-				<div class="section-title">The Experience</div>
+			<div class="row">
+				<div class="col-sm-12">
+					<div class="section-title">The Experience</div>
+				</div>
 			</div>
-			<div class="col-sm-12">
-					
-				<div class="two-column-feature">
-					<div class="flexrow">
-						<?php
 
-							foreach($exp_posts as $key=>$post){
-								setup_postdata($post);
+			<div class="row">
+				<div class="col-sm-12">
+						
+					<div class="two-column-feature">
+						<div class="flexrow">
+							<?php
 
-								echo '<div class="col-sm-6">';
-								get_template_part('parts/exp-quote-card');
-								echo '</div>';
-							}
+								foreach($exp_posts as $key=>$post){
+									setup_postdata($post);
 
-							wp_reset_postdata();
-							
+									echo '<div class="col-sm-6">';
+									get_template_part('parts/exp-quote-card');
+									echo '</div>';
+								}
 
-						?>
+								wp_reset_postdata();
+								
+
+							?>
+						</div>
+								
 					</div>
-							
 				</div>
 			</div>
 		</div>
@@ -198,7 +209,12 @@
 	
 	<section id="future">
 		<div class="container">
-			<div class="col-sm-12"><div class="section-title">Your Future</div></div>
+			<div class="row">
+				<div class="col-sm-12">
+					<div class="section-title">Your Future</div>
+				</div>
+			</div>
+			<div class="row">
 				
 				<div class="col-sm-9">
 
@@ -236,41 +252,39 @@
 					
 					<a href="/admission/undergraduate/discover/career-outcomes.html" class="btn btn-arrow-right block blue">More Career Outcomes</a>
 				</div>
+			
 			</div>
 
+			<?php
 
+				$alumni_quotes_field = get_field('related_quotes');
 
-
-		<?php
-
-			$alumni_quotes_field = get_field('related_quotes');
-
-			if(!empty($alumni_quotes_field)){
-				shuffle($alumni_quotes_field);
-			
-				$displayed_alumni_quotes = array_slice($alumni_quotes_field, 0, 1);
+				if(!empty($alumni_quotes_field)){
+					shuffle($alumni_quotes_field);
 				
-				$alumni_posts = get_posts(array(
-					'post_type' => 'quotes',
-					'include' => $displayed_alumni_quotes
-				));
+					$displayed_alumni_quotes = array_slice($alumni_quotes_field, 0, 1);
+					
+					$alumni_posts = get_posts(array(
+						'post_type' => 'quotes',
+						'include' => $displayed_alumni_quotes
+					));
 
-				if(!empty($alumni_posts)){
-					foreach($alumni_posts as $key=>$post){
-						setup_postdata($post);
-						
-						get_template_part('parts/alumni-quote-card');
+					if(!empty($alumni_posts)){
+						foreach($alumni_posts as $key=>$post){
+							setup_postdata($post);
+							
+							get_template_part('parts/alumni-quote-card');
+						}
 					}
+				
+					wp_reset_postdata();
+
 				}
-			
-				wp_reset_postdata();
 
-			}
-
-		?>
+			?>
 
 		
-		
+		</div>
 	</section>
 	
 	<?php } ?>
