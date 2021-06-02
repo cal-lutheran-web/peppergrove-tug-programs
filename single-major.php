2ffb2016fa4366aecf121817137e0ecaa0e2ee42
Scott Smith <stsmith@callutheran.edu>
2021-06-02 14:03:38 -0700

    removed tagline

--- single-major.php
+++ single-major.php
@@ -33,7 +33,7 @@
 							
 				<div class="<?php echo $summary_css_class; ?> program-summary">
 					
-					<?php echo get_field('tagline') ? '<p class="intro">'.get_field('tagline').'</p>' : ''; ?>
+					<?php //echo get_field('tagline') ? '<p class="intro">'.get_field('tagline').'</p>' : ''; ?>
 					<?php the_field('summary'); ?>
 					
 				</div>
