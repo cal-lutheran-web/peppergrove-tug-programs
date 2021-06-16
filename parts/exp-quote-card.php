813f2da1692e889f453f320a0112bab7a6c20b93
Scott Smith <stsmith@callutheran.edu>
2021-06-16 13:55:34 -0700

    added support for videos in quotes

--- parts/exp-quote-card.php
+++ parts/exp-quote-card.php
@@ -1,10 +1,19 @@
 <?php
 
-	$img_data = wp_get_attachment_url(get_post_thumbnail_id($post->ID));
 	$quote = get_field('quote');
+
+	$img_data = wp_get_attachment_url(get_post_thumbnail_id($post->ID));
+	$video_id = get_field('youtube_id');
+
+	if($video_id !== ''){
+		$media = '<iframe width="560" height="315" src="//www.youtube.com/embed/'.$video_id.'?rel=0" frameborder="0" allowfullscreen></iframe>';
+	} else if($img_data !== false){
+		$media = '<img src="'.$img_data.'">';
+	}
+	
 
 ?>
 
 
-<div class="wrap-16x9"><img src="<?php echo $img_data; ?>"></div>
+<div class="wrap-16x9"><?php echo $media; ?></div>
 <?php echo $quote; ?>
